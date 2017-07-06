99da0ad5b829a999412af31ab93f816c4044d8f5
Jonathas Almeida <jonathasarts@gmail.com>
2017-07-06 18:26:44 -0300

    Adicionados arquivo de CONF_IMPLANTÇÃO e script para criação do banco

--- arquivos/BD/Script_BDabpresa.sql
+++ arquivos/BD/Script_BDabpresa.sql
@@ -24,7 +24,7 @@ CREATE TABLE praticas (
  id SERIAL PRIMARY KEY,
  titulo_pratica VARCHAR NOT NULL,
  descricao_pratica VARCHAR NOT NULL,
- categorias_id INTEGER NOT NULL REFERENCES categorias(id) --ver a possibilidade de apagar categoria com praticas associadas
+ categorias_id INTEGER NOT NULL REFERENCES categorias(id) --ver a possibilidade de deletar categoria com praticas associadas
 );
 
 CREATE TABLE arquivos (
@@ -48,8 +48,9 @@ CREATE TABLE praticas_tags (
  tags_id INTEGER NOT NULL REFERENCES tags(id)
 );
 
+---------------------------------------------------------------------
 /*INSERTS*/
-/*INSERTS*/
+---------------------------------------------------------------------
 INSERT INTO categorias (id, titulo_categoria) VALUES (1, 'RUP');
 INSERT INTO categorias (id, titulo_categoria) VALUES (2, 'XP');
 INSERT INTO categorias (id, titulo_categoria) VALUES (3, 'SCRUM');
@@ -58,6 +59,7 @@ INSERT INTO tags (id, descricao_tag) VALUES (1, 'GERENCIA');
 INSERT INTO tags (id, descricao_tag) VALUES (2, 'DESENVOLVIMENTO');
 INSERT INTO tags (id, descricao_tag) VALUES (3, 'REUNIOES');
 INSERT INTO tags (id, descricao_tag) VALUES (4, 'XP');
+INSERT INTO tags (id, descricao_tag) VALUES (5, 'TESTES');
 
 INSERT INTO tipo_arquivo (id, descricao_tipo_arquivo) VALUES (1, 'PDF');
 INSERT INTO tipo_arquivo (id, descricao_tipo_arquivo) VALUES (2, 'PNG');
@@ -70,9 +72,12 @@ VALUES (2, 'A002', 'Arquivo A002', 'descricao do arquivo A002', '/diretorio/test
 INSERT INTO arquivos (id, cod_arquivo, titulo_arquivo, descricao_arquivo, path_arquivo, tipo_arquivo_id) 
 VALUES (3, 'B001', 'Arquivo B001', 'descricao do arquivo B001', '/diretorio/teste/B001.png', 2);
 
-INSERT INTO praticas (id, titulo_pratica, categorias_id) VALUES (1, 'Planning Pocker', 3);
-INSERT INTO praticas (id, titulo_pratica, categorias_id) VALUES (2, 'Reunios Diarias', 2);
-INSERT INTO praticas (id, titulo_pratica, categorias_id) VALUES (3, 'Desenvolvimento em Pares', 2);
+INSERT INTO praticas (id, titulo_pratica, descricao_pratica, categorias_id) VALUES (1, 'Planning Pocker', 'Descrição da boa prática Planning Pocker', 3);
+INSERT INTO praticas (id, titulo_pratica, descricao_pratica, categorias_id) VALUES (2, 'Reunios Diarias', 'Descrição da boa prática Reunios Diarias', 2);
+INSERT INTO praticas (id, titulo_pratica, descricao_pratica, categorias_id) VALUES (3, 'Desenvolvimento em Pares', 'Descrição da boa prática Desenvolvimento em Pares', 2);
+INSERT INTO praticas (id, titulo_pratica, descricao_pratica, categorias_id) VALUES (4, 'Disponibilidade do Cliente', 'Descrição da boa prática Disponibilidade do Cliente', 2);
+INSERT INTO praticas (id, titulo_pratica, descricao_pratica, categorias_id) VALUES (5, 'Small Releases', 'Descrição da boa prática Small Releases', 2);
+INSERT INTO praticas (id, titulo_pratica, descricao_pratica, categorias_id) VALUES (6, 'Testes de Aceitação', 'Descrição da boa prática Testes de Aceitação', 2);
 
 INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (1, 1, 1);
 INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (2, 1, 3);
@@ -80,7 +85,20 @@ INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (3, 2, 3);
 INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (4, 2, 4);
 INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (5, 3, 4);
 INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (6, 3, 2);
+INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (7, 4, 3);
+INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (8, 4, 4);
+INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (9, 5, 4);
+INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (10, 6, 4);
+INSERT INTO praticas_tags (id, praticas_id, tags_id) VALUES (11, 6, 5);
 
 INSERT INTO praticas_arquivos (id, praticas_id, arquivos_id) VALUES (1, 1, 1);
 INSERT INTO praticas_arquivos (id, praticas_id, arquivos_id) VALUES (2, 1, 2);
 INSERT INTO praticas_arquivos (id, praticas_id, arquivos_id) VALUES (3, 2, 3);
+INSERT INTO praticas_arquivos (id, praticas_id, arquivos_id) VALUES (4, 4, 2);
+INSERT INTO praticas_arquivos (id, praticas_id, arquivos_id) VALUES (5, 5, 2);
+INSERT INTO praticas_arquivos (id, praticas_id, arquivos_id) VALUES (6, 6, 2);
+
+INSERT INTO usuarios (id, nome, username, password) VALUES (1, 'Jonathas Almeida', 'jonathas.almeida', 12345);
+INSERT INTO usuarios (id, nome, username, password) VALUES (2, 'Alber Jonathas', 'alber.jonathas', 12345);
+INSERT INTO usuarios (id, nome, username, password) VALUES (3, 'Samyra Almeida', 'samyra.almeida', 12345);
+INSERT INTO usuarios (id, nome, username, password) VALUES (4, 'Heremita Brasileiro', 'heremita.brasileiro', 12345);
--- arquivos/CONFIG_IMPLANTAÇÃO.sql
+++ arquivos/CONFIG_IMPLANTAÇÃO.sql
@@ -2,4 +2,46 @@
 /*---------------*/ CONFIGURAÇÃO PARA IMPLANTAÇÃO /*---------------*/
 =====================================================================
 
-SLIM PHP ==> https://www.slimframework.com/
+Instalar os softwares:
+> XAMP 7.1.6
+> PostgreSQL 9.4
+
+---------------------------------------------------------------------
+						INSTALAÇÃO DO SLIM PHP
+---------------------------------------------------------------------
+1 - no XAMP edite o arquivo do apache (no botão 'config'), o arquivo 
+php.ini removendo o ';' nas linhas:
+
+	extension=php_openssl.dll,
+	extension=php_curl.dll,
+	extension=php_sockets.dll
+
+2 - Faça o download e instale o Composer-Setup.exe 
+	https://getcomposer.org/Composer-Setup.exe);
+
+3 - Clone o projeto no diretório 'htdocs' do XAMP em C:\xampp\htdocs;
+
+4 - Crie um novo banco no postgreSQL com o nome 'bd_abpresa' e usuario 
+'postgres' e senha 'postgres'.
+	/*(OBS. Esses dados são provisórios, para modificar os dados do 
+	banco basta editar o arquivo init.php na raiz da aplicação);*/
+
+5 - Rode os scripts de criação e insert dos dados nas tabelas no 
+Banco criado (abpresa). Os scripts estão no diretório do projeto 
+abpresa\arquivos\BD\Script_BDabpresa.sql;
+
+6 - Inicie o apache no XAMP e acesse o projeto em 
+http://localhost/abpresa/;
+
+7 - Pronto! :).
+
+
+---------------------------------------------------------------------
+							DOCUMENTAÇÃO
+---------------------------------------------------------------------
+SLIM PHP 	==> https://www.slimframework.com/
+COMPOSER 	==> https://getcomposer.org/download/
+POSTGRESQL 	==> https://www.postgresql.org/
+XAMP 		==> https://www.apachefriends.org/pt_br/index.html
+---------------------------------------------------------------------
+---------------------------------------------------------------------
